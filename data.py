0994f4d5724204c4b814197643da74c327d645e0
SAYAN SAHA <88489941+SAYANSAHA29@users.noreply.github.com>
2021-08-06 11:21:32 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,8 @@
+lst = [
+    ["https://us02web.zoom.us/j/5581802885?pwd=QWpwOFU4alc3WXhRM3dqK1BFbmdqUT09", "09:16", "09:40"],
+    ["https://us02web.zoom.us/j/5581802885?pwd=QWpwOFU4alc3WXhRM3dqK1BFbmdqUT09", "09:50", "10:30"],
+    ["https://us02web.zoom.us/j/5581802885?pwd=QWpwOFU4alc3WXhRM3dqK1BFbmdqUT09", "10:40", "11:20"],
+    ["https://us02web.zoom.us/j/5581802885?pwd=QWpwOFU4alc3WXhRM3dqK1BFbmdqUT09", "11:30", "12:10"],
+    ["https://us02web.zoom.us/j/5581802885?pwd=QWpwOFU4alc3WXhRM3dqK1BFbmdqUT09", "12:20", "13:00"]
+]
+
